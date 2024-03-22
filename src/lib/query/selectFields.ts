da15449a1e3b6c3022173659efba8bfe85b0c5f3
JoshBot-Debug <joshuamyerswebdev@gmail.com>
2024-03-22 21:10:28 +0530

    reverted to before lookup

--- src/lib/query/selectFields.ts
+++ src/lib/query/selectFields.ts
@@ -1,10 +1,9 @@
 import { type ORS } from "../types";
 
-
 /**
  * This method does not mutate the object,
  * It creates a copy and returns a new object with the selected fields
- * 
+ *
  * @param fields Fields to select from the object
  * @param schema The object schema
  * @param object The object to select the fields from.
@@ -13,32 +12,23 @@ import { type ORS } from "../types";
 export default function selectFields<
   N extends string,
   O extends Record<string, any>
->(
-  fields: ORS.SelectOptions<N, O>["fields"],
-  object: O
-) {
-
+>(fields: ORS.SelectOptions<N, O>["fields"], object: O) {
   if (!object) return null;
 
   const result: O = {} as O;
 
-  function mapOverFields(fields: (keyof O)[]) {
-    fields
-      .forEach((field) => {
-        const value = object[field];
-        if (value === undefined) return;
-        result[field] = value;
-      })
-  }
+  const selectedFields = fields === "*" ? Object.keys(object) : fields;
 
   /**
    * If we are returning all fields, select everything and all relations,
    * replace them with their primary key
    */
-  if (fields === "*")
-    mapOverFields(Object.keys(object))
-  else
-    mapOverFields(fields as string[])
+  for (let i = 0; i < selectedFields.length; i++) {
+    const field = selectedFields[i];
+    const value = object[field];
+    if (value === undefined) continue;
+    result[field] = value;
+  }
 
-  return result
+  return result;
 }
